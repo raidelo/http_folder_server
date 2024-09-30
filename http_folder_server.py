b7f48a03385f5233aa97f1638af995db4dbfadff
raidelo <raidelosix@gmail.com>
2024-09-30 01:52:07 -0400

    Agregada funcionalidad para guardar los logs en un archivo

--- http_folder_server.py
+++ http_folder_server.py
@@ -2,36 +2,59 @@ from http import server
 from os import getcwd
 
 class MainHandler(server.SimpleHTTPRequestHandler):
-    def __init__(self, request, address, server, directory=None, logging=0):
+    def __init__(self, request, address, server, directory=None, logs_file=None):
         self.directory = directory or getcwd()
-        self.logging = logging
+        self.logs_file = logs_file
         super().__init__(request, address, server, directory=directory)
     def log_message(self, *args, **kwargs) -> None:
-        if self.logging>=2:
-            super().log_message(*args, **kwargs)
+        message = args[0] % args[1:]
+        msg = "%s - - [%s] %s" %(
+            self.address_string(),
+            self.log_date_time_string(),
+            message.translate(self._control_char_table)
+            )
+        self.server.logf(msg)
 
 class MainServer(server.ThreadingHTTPServer):
-    def __init__(self, address=('0.0.0.0', 80), directory=None, logging=0):
+    def __init__(self, address=('0.0.0.0', 80), directory=None):
         self.directory = directory
-        self.logging = logging
+        self.logs_file = None
+        self.logf = print
         host = '::' if address[0] == '0.0.0.0' else address[0]
         url_host = f'[{host}]' if ':' in host else host
         port = address[1]
-        if logging>=1:
-            print('Serving HTTP on {host} port {port} (http://{url_host}:{port}/) ...'.format(host=host, url_host=url_host, port=port))
+        print('Serving HTTP on {host} port {port} (http://{url_host}:{port}/) ...'.format(host=host, url_host=url_host, port=port))
         super().__init__(address, self.handler)
 
     def handler(self, request, address, server):
-        MainHandler(request, address, server, directory=self.directory, logging=self.logging)
-    
+        MainHandler(request, address, server, directory=self.directory, logs_file=self.logs_file)
+
     def start(self):
         try:
             self.serve_forever()
         except KeyboardInterrupt:
-            print('Keyboard interrupt received, exiting.') if self.logging else None
+            print('Keyboard interrupt received, exiting.')
 
-def main(address=('0.0.0.0', 80), directory=None, logging=False):
-    _server = MainServer(address, directory, logging)
+    def log_dumper(self, data):
+        print(data)
+        self.logs_file.write(data.encode() + b'\n')
+        self.logs_file.flush()
+
+    def set_logs_file(self, logs_file:str):
+        try:
+            self.logs_file = open(logs_file, "wb")
+            self.logf = self.log_dumper
+        except:
+            print("error: failed to open the log file")
+
+    def __del__(self):
+        if self.logs_file:
+            self.logs_file.close()
+
+def main(address=('0.0.0.0', 80), directory=None, logs_file=None):
+    _server = MainServer(address, directory)
+    if logs_file:
+        _server.set_logs_file(logs_file)
     _server.start()
 
 if __name__ == '__main__':
@@ -43,11 +66,11 @@ if __name__ == '__main__':
     parser.add_argument('port', default=80, type=int, nargs='?',
                         help='bind to this port '
                              '(default: %(default)s)')
-    parser.add_argument('-v', '--verbose', action='count', default=0,
-                        help='verbose '
+    parser.add_argument('--logs-file', default='shttp_server.log', dest='logs_file',
+                        help='logs file '
                              '(default: %(default)s)')
     parser.add_argument('-d', '--directory', default=getcwd(), dest='directory',
                         help='serve this directory '
                              '(default: current directory)')
     args = parser.parse_args()
-    main((args.address, args.port), args.directory, args.verbose)
+    main((args.address, args.port), args.directory, args.logs_file)
